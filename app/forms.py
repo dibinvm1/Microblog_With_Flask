b6c1500928c8c106ab733cb46f6d8d3d00d7a653
Dibin V M <dibinvm1@gmail.com>
2020-08-09 22:07:52 +0530

    Added profile section

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo,Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -26,3 +26,9 @@ class RegristrationForm(FlaskForm):
         user = User.query.filter_by(email = email.data).first()
         if user is not None:
             raise ValidationError('Please Use a different Email address')
+
+
+class EditProfileForm(FlaskForm):
+    uName = StringField('User Name', validators=[DataRequired()])
+    aboutMe = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
